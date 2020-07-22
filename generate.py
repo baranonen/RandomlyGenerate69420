25d98b3ed029d6c4707526f6d430d5932ecaf126
Anton <goldtom1423@gmail.com>
2020-07-22 13:25:30 +0200

    Refactor code
    Import only ```randint```
    Move code inside function
    Refactor f-strings and increments
    Add strings inside ```input()```
    Add line breaks
    Format variables that are used once
    Add possibility to try again
    Accept uppercase 'y'
    Clear output after each ```while``` cycle

--- generate.py
+++ generate.py
@@ -1,19 +1,23 @@
-from random import *
+from random import randint
 
 print("Randomly Generate 69420")
 print("(c) Baran Önen 2020")
 print("This program will generate random 5-digit numbers until it generates 69420")
-print("To start, type y")
 
-answer = input()
 
-if answer == "y":
+def run_random():
     trialnumber = 0
     number = 0
 
     while number != 69420:
         number = randint(10000, 99999)
-        print(number)
-        trialnumber = trialnumber + 1
+        print(number, end="\r")
+        trialnumber += 1
 
-    print("Done in " + str(trialnumber) + " tries")
+    return f"\nDone in {trialnumber} tries."
+
+
+if input("\nStart? [Y/N]: ").lower() == "y":
+    print(run_random())
+    while input("\nTry again? [Y/N]: ").lower() == 'y':
+        print(run_random())
